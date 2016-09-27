2ab5bb1e2303d1a9633a0be00ab18f634440522e
geonhui <cptcpt123@gmail.com>
2016-09-28 00:51:44 +0900

    구조체구현

--- StudyCode/4Week/GeonHuiex7/GeonHuinex7/main.m
+++ StudyCode/4Week/GeonHuiex7/GeonHuinex7/main.m
@@ -11,7 +11,16 @@
 int main(int argc, const char * argv[]) {
 
     
-    Person *me = [[Person alloc] init];
+    //Person *me = [[Person alloc] init];
+    //구조체를 불러오고
+    struct character to;
+    //값을 지정해주고
+    to.height = 30;
+    to.weight = 180;
+    //그 값을 cgfloat으로 받고 받은 변수로 표현
+    CGFloat myHeight = to.height;
+    CGFloat myWeight = to.weight;
+    NSLog(@"%lf, %lf", myHeight, myWeight);
     
     
     
